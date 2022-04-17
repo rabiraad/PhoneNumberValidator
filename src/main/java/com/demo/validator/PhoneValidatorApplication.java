5f3ec226023238e6855a1463f4663d6a91c95afa
Rebecca Abi Raad <rebecca.abiraad@gotocme.com>
2022-04-17 21:52:23 +0300

    Add service registry and swagger documentation

--- src/main/java/com/demo/validator/PhoneValidatorApplication.java
+++ src/main/java/com/demo/validator/PhoneValidatorApplication.java
@@ -2,8 +2,12 @@ package com.demo.validator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class PhoneValidatorApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/demo/validator/controller/PhoneValidatorController.java
+++ src/main/java/com/demo/validator/controller/PhoneValidatorController.java
@@ -2,6 +2,7 @@ package com.demo.validator.controller;
 
 import com.demo.validator.model.PhoneNumberDetails;
 import com.demo.validator.service.PhoneValidatorService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/validators/")
+@RequestMapping("/api/v1/validators/")
 @Slf4j
 public class PhoneValidatorController {
 
   @Autowired private PhoneValidatorService phoneValidatorService;
 
   @GetMapping("/{phoneNumber}")
+  @ApiOperation(
+      value =
+          "validates the input phone number and returns the country code, country name, and operator name")
   public PhoneNumberDetails validatePhoneNumber(@PathVariable String phoneNumber) {
     log.info("PhoneValidatorController -- validatePhoneNumber: validating " + phoneNumber);
     return phoneValidatorService.validatePhoneNumber(phoneNumber);
